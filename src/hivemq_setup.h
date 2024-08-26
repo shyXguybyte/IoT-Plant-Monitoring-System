0431be5734bb07f05522fc9f450c66c137bcbd46
Shehab Magdy Elsayed <shehab2003magdy@gmail.com>
2024-08-26 20:04:09 +0300

    Delete src directory

--- src/hivemq_setup.h
+++
@@ -1,7 +0,0 @@
-#ifndef __MYCODE_H__
-#define __MYCODE_H__
-
-void send_hive(const char* obj);
-void hivemq_setup();
-
-#endif
--- src/wifi_setup.h
+++
@@ -1,6 +0,0 @@
-#ifndef __MYCODE_H__
-#define __MYCODE_H__
-
-void start_wifi();
-
-#endif
