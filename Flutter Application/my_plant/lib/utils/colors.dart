a95ff62b651393d30a0e0eb704caf88c783b0673
Shehab Magdy Elsayed <shehab2003magdy@gmail.com>
2024-09-12 15:45:27 +0300

    Update colors.dart , added more constants colors

--- Flutter Application/my_plant/lib/utils/colors.dart
+++ Flutter Application/my_plant/lib/utils/colors.dart
@@ -4,4 +4,7 @@ class Constants {
   //Primary color
   static var primaryColor = const Color(0xff296e48);
   static var blackColor = Colors.black54;
+  static const Color Mygreen = Colors.green;
+  static Color lightgreen = Colors.lightGreen.shade100;
+  static const Color secondaryColor = Colors.lightGreen;
 }
